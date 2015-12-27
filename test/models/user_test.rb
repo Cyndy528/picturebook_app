edecd3d1fdcf23a9170975881aeda13397d2f5ba
Cynthia Salinas <csalinas123@gmail.com>
2015-12-27 10:01:46 -0800

    add tests for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ 	test "a user should enter a first name" do 
+	user = User.new
+	assert !user.save
+	# asserting that the first name is not empty 
+	assert !user.errors[:first_name].empty? 
+ 	end 
+
+  	test "a user should enter a last name" do 
+	user = User.new
+	assert !user.save
+	# asserting that the first name is not empty 
+	assert !user.errors[:last_name].empty? 
+ 	end 
+
+ 	test "a user should enter a profile name" do 
+	user = User.new
+	assert !user.save
+	# asserting that the first name is not empty 
+	assert !user.errors[:profile_name].empty? 
+ 	end 
+
+ 	test "a user should have a unique profile name" do 
+ 	user = User.new
+ 	user.profile_name = 'cynthiasalinas'
+ 	user.first_name = 'cynthia'
+ 	user.last_name = 'salinas'
+ 	user.email= 'cynthia@email.com'
+ 	user.password = 'password'
+ 	user.password_confirmation = 'password'
+
+ 	assert !user.save
+ 	assert !user.errors[:profile_name].empty? 
+    end
+
+    test "a user should have a profile name without spaces" do 
+    user.profile_name = "My Profile with Spaces"
+
+    assert !user.save
+    assert !user.errors[:profile_name].empty? 
+    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+	end
 end
